65ad6df5b8a12677a1a5bdcea7f0679272ac0f90
Andy Baird <andybaird@gmail.com>
2016-02-26 16:24:12 -0600

    Column / parser framework

---
+++ columns.js
@@ -0,0 +1,1 @@
+const column = require('./lib/column');
--- parse.js
+++ parse.js
@@ -1,3 +1,8 @@
-const heroprotocoljs = require('heroprotocoljs');
+const heroprotocol = require('heroprotocoljs');
+const file = '../replays/Tomb of the Spider Queen (120).StormReplay';
+const details = heroprotocol.get(heroprotocol.DETAILS, file);
+const gameEvents = heroprotocol.get(heroprotocol.GAME_EVENTS, file);
+const trackerEvents = heroprotocol.get(heroprotocol.TRACKER_EVENTS, file);
+const attributeEvents = heroprotocol.get(heroprotocol.ATTRIBUTES_EVENTS, file);
 
-console.log(process);
+const parser = require('./parser');

4750b60dc4b80fe5bd9c99d8382585dd1dfc6f90
Andy Baird <andybaird@gmail.com>
2016-03-01 16:39:06 -0600

    Fix bad syntax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 "use strict";
-const parser = export.parser = require('./lib/parser').Parser;
-const plugins = export.plugins = require('./lib/plugins').plugins;
-const heroesjson = export.heroesjson = require('./lib/heroes');
+const parser = exports.parser = require('./lib/parser').Parser;
+const plugins = exports.plugins = require('./lib/plugins').plugins;
+const heroesjson = exports.heroesjson = require('./lib/heroes');

78f879b6d4b3ddc1797c0234435a420683c031c8
Andy Baird <andybaird@gmail.com>
2016-03-01 16:32:12 -0600

    Entry point for module

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+const parser = export.parser = require('./lib/parser').Parser;
+const plugins = export.plugins = require('./lib/plugins').plugins;
+const heroesjson = export.heroesjson = require('./lib/heroes');
